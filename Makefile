186056281ccf3974c629f76612913def73afaad3
Tomas Figueroa Estrada <tfiguero@car2s4.42barcelona.com>
2023-12-27 17:16:37 +0100

    nikelaoo

---
+++ Makefile
@@ -0,0 +1,63 @@
+NAME_CLIENT = client
+NAME_SERVER = server
+
+#########
+RM = rm -f
+CC = cc
+CFLAGS = -Werror -Wextra -Wall #-g -fsanitize=address
+#########
+
+#########
+FILES_CLIENT = client
+
+FILES_SERVER = server
+
+SRC_CLIENT = $(addsuffix .c, $(FILES_CLIENT))
+
+SRC_SERVER = $(addsuffix .c, $(FILES_SERVER))
+
+vpath %.c srcs
+#########
+
+#########
+OBJ_DIR = objs
+OBJ_CLIENT = $(addprefix $(OBJ_DIR)/, $(SRC_CLIENT:.c=.o))
+DEP_CLIENT = $(addsuffix .d, $(basename $(OBJ_CLIENT)))
+OBJ_SERVER = $(addprefix $(OBJ_DIR)/, $(SRC_SERVER:.c=.o))
+DEP_SERVER = $(addsuffix .d, $(basename $(OBJ_SERVER)))
+#########
+
+#########
+$(OBJ_DIR)/%.o: %.c
+	@mkdir -p $(@D)
+	@${CC} $(CFLAGS) -MMD -c $< -o $@
+
+all:
+	@$(MAKE) $(NAME_CLIENT) $(NAME_SERVER) --no-print-directory
+	@echo "EVERYTHING DONE✌️   "
+
+$(NAME_CLIENT): $(OBJ_CLIENT)
+	$(CC) $(CFLAGS) $(OBJ_CLIENT) -o $(NAME_CLIENT)
+
+$(NAME_SERVER): $(OBJ_SERVER)
+	$(CC) $(CFLAGS) $(OBJ_SERVER) -o $(NAME_SERVER)
+
+# bonus:
+# 	@$(MAKE) $(NAME_BONUS) --no-print-directory
+
+clean:
+	$(RM) $(OBJ_CLIENT) $(DEP_CLIENT) $(OBJ_SERVER) $(DEP_SERVER) --no-print-directory
+	$(RM) -r $(OBJ_DIR) --no-print-directory
+	@echo "OBJECTS REMOVED😭   "
+
+fclean: clean
+	$(RM) $(NAME_CLIENT) $(NAME_SERVER) --no-print-directory
+	@echo "EVERYTHING REMOVED😭   "
+
+re:	fclean all
+
+.PHONY: all clean fclean re
+
+-include $(DEP)
+
+#########
---
+++ srcs/client.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfiguero <tfiguero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 04:31:05 by tfiguero          #+#    #+#             */
+/*   Updated: 2023/09/15 07:12:56 by tfiguero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+int	ft_atoi(const char	*str)
+{
+	int	ret;
+	int	sign;
+
+	sign = 1;
+	ret = 0;
+	while (*str == 9 || *str == 10 || *str == 11
+		|| *str == 12 || *str == 13 || *str == 32)
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+		{
+			sign = -sign;
+		}
+		str ++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		ret = (ret * 10) + *str - 48;
+		str ++;
+	}
+	return (ret * sign);
+}
+
+void	ft_send(int ch, int pid, int j)
+{
+	while (j < 8)
+	{
+		if ((ch & (0x01 << j)) != 0)
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				write(1, "Signal error\n", 13);
+				exit (0);
+			}
+			usleep(150);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				write(1, "Signal error\n", 13);
+				exit (0);
+			}
+			usleep(150);
+		}
+		usleep(200);
+		j++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		while (argv[2][i] != '\0')
+		{
+			ft_send(argv[2][i], pid, 0);
+			i++;
+		}
+		ft_send('\n', pid, 0);
+	}
+	else
+	{
+		write(1, "Error: wrong arguments.\n", 24);
+		write(1, "Try: ./client <Server PID> <\"MESSAGE\">\n", 39);
+		return (1);
+	}
+	return (0);
+}
